bb8dbb7de2d70a69552e05a373dbd64e6c79fd45
Nick Clifford <nickclifford@gmail.com>
2021-03-29 14:05:24 +0100

    index.php update

--- index.php
+++ index.php
@@ -5,8 +5,11 @@ $query = $db->prepare('SELECT * FROM `chaircollection`;');
 $query->setFetchMode(PDO::FETCH_ASSOC);
 $query->execute();
 
-echo '<ul>';
-foreach ($query as $chair) {
-    echo '<li>' . $chair['chairname'] . '</li>';
-}
-echo '</ul>';
+
+
+    echo '<ul>';
+    foreach ($query as $chair) {
+        echo '<li>The ' . $chair['chairname'] . ' was designed by ' . $chair['designer'] . ' in ' . $chair['designyear'] . '.</li>';
+    }
+    echo '</ul>';
+
