b3bddfc869250d66c56c6f20a0780784afb0957c
Nick Clifford <nickclifford@gmail.com>
2021-03-29 14:56:40 +0100

    starting to try and put a functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,4 @@
 <?php
+
+$db = new PDO('mysql:host=db; dbname=chairs', 'root', 'password');
+$db ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
+<?php ?>
+
+<h2>The Chair Collector</h2>
+
 <?php
+
 $db = new PDO('mysql:host=db; dbname=chairs', 'root', 'password');
 $query = $db->prepare('SELECT * FROM `chaircollection`;');
-
 $query->setFetchMode(PDO::FETCH_ASSOC);
 $query->execute();
-
-
 
     echo '<ul>';
     foreach ($query as $chair) {
